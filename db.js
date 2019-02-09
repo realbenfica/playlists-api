3445ef0753a41deb28d9a7630a07c762f97e71fc
“ALEX <alexgtr84@gmail.com>
2019-02-09 20:44:50 +0100

    Add last try

--- db.js
+++ db.js
@@ -4,7 +4,6 @@ const connectionString = process.env.DATABASE_URL || 'postgres://postgres:secret
 const sequelize = new Sequelize(connectionString, { define: { timestamps: false } })
 
 sequelize.sync()
-  // to be an argument for previous function sync() {force:true}
   .then(() => {
     console.log('Sequelize updated database schema')
   })
