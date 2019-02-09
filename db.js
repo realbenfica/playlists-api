2b98586e64c243aff7b0a742e01d3c033ab18bc0
“ALEX <alexgtr84@gmail.com>
2019-02-09 19:45:15 +0100

    Revert change db.js 2

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize')
 
-const connectionString = process.env.DATABASE_URL || port
+const connectionString = process.env.DATABASE_URL || 'postgres://postgres:secret@localhost:5432/postgres'
 const sequelize = new Sequelize(connectionString, { define: { timestamps: false } })
 
 sequelize.sync()
@@ -11,5 +11,3 @@ sequelize.sync()
   .catch(console.error)
 
 module.exports = sequelize
-
-// 'postgres://postgres:secret@localhost:5432/postgres'
